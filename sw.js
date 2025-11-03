0277c805606572e17c2fdfc90ebd7a512af99ecd
Смирнов Николай <nn.smir@gmail.com>
2025-11-03 20:38:59 +0300

    Fix route sharing: use /api/save-route endpoint

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Версия приложения - ОБНОВЛЯТЬ ПРИ КАЖДОМ ДЕПЛОЕ!
-const APP_VERSION = '1.12.2';
+const APP_VERSION = '1.12.3';
 const CACHE_NAME = `rogein-v${APP_VERSION}`;
 
 const urlsToCache = [
